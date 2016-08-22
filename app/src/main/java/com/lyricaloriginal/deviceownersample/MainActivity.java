77f917a7a5c1b86b1d8f6e06cb8aeb18e882916a
shinichitanimoto <s.tanimoto@japantaxi.co.jp>
2016-08-23 01:50:34 +0900

    M Permissionの設定に関する実装が完了。

--- app/src/main/java/com/lyricaloriginal/deviceownersample/MainActivity.java
+++ app/src/main/java/com/lyricaloriginal/deviceownersample/MainActivity.java
@@ -1,12 +1,27 @@
 package com.lyricaloriginal.deviceownersample;
 
+import android.Manifest;
 import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_STORAGE_PERMISSION = 1;
 
     private Button mInstallLocalAppBtn;
     private Button mClearDeviceOwnerBtn;
@@ -22,6 +37,48 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+            }
+        });
+        findViewById(R.id.policy_auto_granted_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mDpm.isDeviceOwnerApp(getPackageName())) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        ComponentName cn = new ComponentName(MainActivity.this, MyDeviceAdminReceiver.class);
+                        mDpm.setPermissionPolicy(cn, DevicePolicyManager.PERMISSION_POLICY_AUTO_GRANT);
+                        Toast.makeText(MainActivity.this, "Policy Auto Granted", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+        findViewById(R.id.policy_prompt_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mDpm.isDeviceOwnerApp(getPackageName())) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        /**
+                         * 元のM Permissionのポリシーに戻すときに使う
+                         */
+                        ComponentName cn = new ComponentName(MainActivity.this, MyDeviceAdminReceiver.class);
+                        mDpm.setPermissionPolicy(cn, DevicePolicyManager.PERMISSION_POLICY_PROMPT);
+                        Toast.makeText(MainActivity.this, "Policy Prompt", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+        findViewById(R.id.show_dcim_file_list_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (PermissionChecker.checkSelfPermission(
+                        MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                                    Manifest.permission.READ_EXTERNAL_STORAGE
+                            },
+                            REQUEST_CODE_STORAGE_PERMISSION);
+                    return;
+                }
+                showDcimList();
             }
         });
         mClearDeviceOwnerBtn = (Button) findViewById(R.id.clear_device_owner_btn);
@@ -44,8 +101,34 @@ public class MainActivity extends AppCompatActivity {
         setButtonEnable(mDpm.isDeviceOwnerApp(getPackageName()));
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+
+        if (requestCode == REQUEST_CODE_STORAGE_PERMISSION) {
+            if (grantResults.length == 1 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                showDcimList();
+            }
+        }
+        //  許可されなかった。
+    }
+
+    private void showDcimList() {
+        File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+        String[] dirs = dir.list();
+        if (dirs == null) {
+            Toast.makeText(this, "DCIMフォルダ内に何もありません。", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Toast.makeText(this, TextUtils.join("\n", dirs), Toast.LENGTH_LONG).show();
+    }
+
     private void setButtonEnable(boolean enable) {
         mInstallLocalAppBtn.setEnabled(enable);
         mClearDeviceOwnerBtn.setEnabled(enable);
+    }
+
+    private void requestStoragePermission() {
     }
 }
--- app/src/main/java/com/lyricaloriginal/deviceownersample/MyDeviceAdminReceiver.java
+++ app/src/main/java/com/lyricaloriginal/deviceownersample/MyDeviceAdminReceiver.java
@@ -13,13 +13,5 @@ public class MyDeviceAdminReceiver extends DeviceAdminReceiver {
     @Override
     public void onEnabled(Context context, Intent intent) {
         super.onEnabled(context, intent);
-        DevicePolicyManager dpm = (DevicePolicyManager) context.
-                getSystemService(Context.DEVICE_POLICY_SERVICE);
-        if (dpm.isDeviceOwnerApp(context.getPackageName())) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                ComponentName cn = getWho(context);
-                dpm.setPermissionPolicy(cn, DevicePolicyManager.PERMISSION_POLICY_AUTO_GRANT);
-            }
-        }
     }
 }
